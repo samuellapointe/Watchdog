777691151ce9e7db37bd50ee79975daac1f87186
mic3mic3 <mic3mic3@hotmail.com>
2017-02-19 17:34:27 -0500

    Page d'informations d'un serveur, sans vraies valeurs ni serveur pour le moment

--- app/src/main/java/ca/uqac/watchdog/ServerDetailsActivity.java
+++ app/src/main/java/ca/uqac/watchdog/ServerDetailsActivity.java
@@ -2,6 +2,7 @@ package ca.uqac.watchdog;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by Michael Dery on 2017-02-12.
@@ -9,11 +10,13 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class ServerDetailsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serverdetails);
+
+        final TextView textViewRam = (TextView) findViewById(R.id.valueRam);
+        textViewRam.setText("999999 GB");
     }
 
 }
