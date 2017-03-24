67097dd549f13ce5837a6caee1359b8f503b32b7
Knuk <samuellapointe@gmail.com>
2017-03-24 14:05:19 -0400

    Add server dialog

--- app/src/main/java/ca/uqac/watchdog/ServerListActivity.java
+++ app/src/main/java/ca/uqac/watchdog/ServerListActivity.java
@@ -2,10 +2,15 @@ package ca.uqac.watchdog;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,7 +55,42 @@ public class ServerListActivity extends ListActivity {
     }
 
     public void OnAddServerClick(View v) {
-        servers.add(new Server("Test", "https://heartbeat.haruha.ru/api.php"));
+        final Context context = this;
+
+        LayoutInflater li = LayoutInflater.from(context);
+        View promptView = li.inflate(R.layout.prompt_addserver, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editTextServerName = (EditText) promptView.findViewById(R.id.editTextServerName);
+        final EditText editTextServerURL = (EditText) promptView.findViewById(R.id.editTextServerURL);
+        
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AddServer(
+                                editTextServerName.getText().toString(),
+                                editTextServerURL.getText().toString()
+                        );
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Cancel pressed
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void AddServer(String serverName, String serverURL) {
+        servers.add(new Server(serverName, serverURL));
         adapter.notifyDataSetChanged();
     }
 }
